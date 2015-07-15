7740fb11a28f9f9d2420e3bd3bcfddda7e73a74c
Raphael Nestler <raphael.nestler@gmail.com>
2015-07-15 21:36:22 +0200

    add some cursor handling
    Example of how to get the mouse position and react on clicks

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@ use piston::window::WindowSettings;
 use piston::event::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
+use piston::input::{ Button };
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64   // Rotation for the square.
+    rotation: f64,   // Rotation for the square.
+    position: [f64; 2],
+    target: [f64; 2],
 }
 
 impl App {
@@ -22,13 +25,13 @@ impl App {
 
         let square = rectangle::square(0.0, 0.0, 50.0);
         let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
+        let pos = self.position;
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(GREEN, gl);
 
-            let transform = c.transform.trans(x, y)
+            let transform = c.transform.trans(pos[0], pos[1])
                                        .rot_rad(rotation)
                                        .trans(-25.0, -25.0);
 
@@ -40,6 +43,10 @@ impl App {
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
         self.rotation += 2.0 * args.dt;
+
+        let diff = [self.target[0]-self.position[0], self.target[1]-self.position[1]];
+        self.position[0] += diff[0]/2.0*args.dt;
+        self.position[1] += diff[1]/2.0*args.dt;
     }
 }
 
@@ -58,13 +65,25 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0
+        rotation: 0.0,
+        position: [50.0, 50.0],
+        target: [50.0, 50.0],
     };
+
+    let mut cursor = [0.0,0.0];
 
     for e in window.events() {
         if let Some(r) = e.render_args() {
             app.render(&r);
         }
+        if let Some(Button::Mouse(button)) = e.press_args() {
+            println!("Pressed mouse button '{:?}'", button);
+            app.target = cursor;
+        }
+        e.mouse_cursor(|x, y| {
+            cursor = [x, y];
+            println!("Mouse moved '{} {}'", x, y);
+        });
 
         if let Some(u) = e.update_args() {
             app.update(&u);
