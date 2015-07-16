a416f74c697944954a7f7f6161b2381cf53197e8
Raphael Nestler <raphael.nestler@gmail.com>
2015-07-16 07:44:18 +0200

    Move rectangle into its own struct

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,29 @@ use piston::event::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 use piston::input::{ Button };
+use piston::input::mouse::MouseButton;
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
+pub struct Square {
     rotation: f64,   // Rotation for the square.
     position: [f64; 2],
     target: [f64; 2],
+    selected: bool,
+}
+
+impl Square {
+    fn new(position: [f64;2]) -> Square {
+        Square {
+            rotation: 0.0,
+            position: position,
+            target: position,
+            selected: false
+        }
+    }
+}
+
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    square: Square,
 }
 
 impl App {
@@ -22,10 +39,12 @@ impl App {
 
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const BLUE:  [f32; 4] = [0.0, 0.0, 1.0, 1.0];
 
         let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let pos = self.position;
+        let rotation = self.square.rotation;
+        let pos = self.square.position;
+        let selected = self.square.selected;
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
@@ -36,17 +55,21 @@ impl App {
                                        .trans(-25.0, -25.0);
 
             // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            if selected {
+                rectangle(RED, square, transform, gl);
+            } else {
+                rectangle(BLUE, square, transform, gl);
+            }
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+        self.square.rotation += 2.0 * args.dt;
 
-        let diff = [self.target[0]-self.position[0], self.target[1]-self.position[1]];
-        self.position[0] += diff[0]/2.0*args.dt;
-        self.position[1] += diff[1]/2.0*args.dt;
+        let diff = [self.square.target[0]-self.square.position[0], self.square.target[1]-self.square.position[1]];
+        self.square.position[0] += diff[0]/2.0*args.dt;
+        self.square.position[1] += diff[1]/2.0*args.dt;
     }
 }
 
@@ -65,9 +88,7 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-        position: [50.0, 50.0],
-        target: [50.0, 50.0],
+        square: Square::new([50.0, 50.0]),
     };
 
     let mut cursor = [0.0,0.0];
@@ -77,8 +98,11 @@ fn main() {
             app.render(&r);
         }
         if let Some(Button::Mouse(button)) = e.press_args() {
-            println!("Pressed mouse button '{:?}'", button);
-            app.target = cursor;
+            match button {
+                MouseButton::Left  => println!("Pressed left mouse button '{:?}'", button),
+                MouseButton::Right => app.square.target = cursor,
+                _ => println!("Pressed mouse button '{:?}'", button),
+            }
         }
         e.mouse_cursor(|x, y| {
             cursor = [x, y];
