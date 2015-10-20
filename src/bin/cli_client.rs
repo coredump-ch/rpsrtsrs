c272e3bef408ebc9c5713d8a8fbc8d9edf6ab066
Raphael Nestler <raphael.nestler@gmail.com>
2015-10-20 18:16:31 +0200

    Use the new network types in server and cli_client

--- src/bin/cli_client.rs
+++ src/bin/cli_client.rs
@@ -4,14 +4,22 @@ extern crate rpsrtsrs;
 use std::net::TcpStream;
 
 use rpsrtsrs::state::{Game};
+use rpsrtsrs::network::{Message};
 
 use bincode::SizeLimit;
-use bincode::rustc_serialize::decode_from;
+use bincode::rustc_serialize::{decode_from, encode_into};
 use bincode::rustc_serialize::DecodingResult;
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
-    let game_state: DecodingResult<Game> = decode_from(&mut stream, SizeLimit::Infinite);
-    println!("{:?}", game_state);
+
+    encode_into(&Message::ClientHello, &mut stream, SizeLimit::Infinite).unwrap();
+    let world: DecodingResult<Message> = decode_from(&mut stream, SizeLimit::Infinite);
+    println!("{:?}", world);
+
+    loop {
+        let game_state: DecodingResult<Game> = decode_from(&mut stream, SizeLimit::Infinite);
+        println!("{:?}", game_state);
+    }
 }
 
