a3dd3999b4985742b828fe675b6dbb36a37fba18
Raphael Nestler <raphael.nestler@gmail.com>
2015-10-20 18:10:57 +0200

    common: Add World to state
    The world consists of the x- and y-dimensions and the game state.

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,5 @@
 /// The state of a single unit
-#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
+#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug, Clone)]
 pub struct Unit {
     pub position: [u64; 2],
     pub angle: u64,
@@ -8,7 +8,7 @@ pub struct Unit {
 }
 
 /// A player has an ID and consists of 0..N `Unit`s
-#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
+#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug, Clone)]
 pub struct Player {
     pub id: u32,
     pub units: Vec<Unit>,
@@ -24,8 +24,28 @@ impl Player {
 }
 
 /// The whole game consists of N players
-#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
+#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug, Clone)]
 pub struct Game {
     pub players: Vec<Player>,
+}
+
+/// The world consists of an x and y size
+#[derive(RustcEncodable, RustcDecodable, PartialEq, Debug, Clone)]
+pub struct World {
+    pub x: u64,
+    pub y: u64,
+    pub game: Game,
+}
+
+impl World {
+    pub fn new(x: u64, y: u64) -> World {
+        World {
+            x: x,
+            y: y,
+            game: Game{
+                players: vec![],
+            }
+        }
+    }
 }
 
