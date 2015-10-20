789f52d849370a033c523765c18372a3b811b4c4
Raphael Nestler <raphael.nestler@gmail.com>
2015-10-20 18:16:31 +0200

    Use the new network types in server and cli_client

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,38 +1,85 @@
 extern crate bincode;
 extern crate rustc_serialize;
 extern crate rpsrtsrs;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::io::Write;
 use std::ops::Deref;
 use std::sync::{Mutex,Arc};
 use std::thread;
 
-use rpsrtsrs::state::{Game, Player};
+use rpsrtsrs::state::{World, Player};
+use rpsrtsrs::network::{Message};
 
 use bincode::SizeLimit;
-use bincode::rustc_serialize::encode;
+use bincode::rustc_serialize::{decode_from, encode};
+use bincode::rustc_serialize::DecodingResult;
+
+type SafeWorldState = Arc<Mutex<World>>;
+
+fn handle_client(mut stream: TcpStream, world: SafeWorldState) {
+
+    // handle client hello
+    let client_message: DecodingResult<Message> = decode_from(&mut stream, SizeLimit::Bounded(128));
+    match client_message {
+        Ok(message) => {
+            match message {
+                Message::ClientHello => {
+                    let mut world_lock = world.lock().unwrap();
+                    let id = world_lock.game.players.last().map_or(0, |player| player.id+1);
+                    // create new player for the newly connected client
+                    world_lock.game.players.push(Player::new(id));
+
+                    let encoded: Vec<u8> = encode(&Message::ServerHello(
+                            id, world_lock.clone()), SizeLimit::Infinite).unwrap();
+                    stream.write(&encoded).unwrap();
+                },
+                _ => {
+                    println!("Did not receive ClientHello{:?}", message);
+                    let encoded: Vec<u8> = encode(&Message::Error, SizeLimit::Infinite).unwrap();
+                    stream.write(&encoded).unwrap();
+                }
+            }
+        }
+        Err(e) => {
+            println!("{:?}", e);
+            return
+        }
+    }
+
+    // GameState loop
+    loop {
+        let world_lock = world.lock().unwrap();
+        let encoded: Vec<u8> = encode(&world_lock.game, SizeLimit::Infinite).unwrap();
+        match stream.write(&encoded) {
+            Err(e) => {
+                println!("Error: {:?}", e);
+                return;
+            }
+            _ =>  thread::sleep_ms(1000),
+        };
+    }
+}
 
 fn main() {
-    let game = Arc::new(Mutex::new(Game{
-        players: vec![],
-    }));
+    let world = Arc::new(Mutex::new(World::new(800, 600)));
 
     let socket_addr = "127.0.0.1:8080".to_string();
     let tcp_listener = TcpListener::bind(socket_addr.deref()).unwrap();
     println!("Start server: {:?}", tcp_listener);
 
     for stream in tcp_listener.incoming() {
-        let game_clone = game.clone();
-        thread::spawn(move || {
-            let mut stream = stream.unwrap();
-            let mut game_lock = game_clone.lock().unwrap();
-            let id = game_lock.players.last().map_or(0, |player| player.id+1);
-
-            // create new player for the newly connected client
-            game_lock.players.push(Player::new(id));
-
-            let encoded: Vec<u8> = encode(game_lock.deref(), SizeLimit::Infinite).unwrap();
-            stream.write(&encoded).unwrap();
-        });
+        match stream {
+            Ok(stream) => {
+                let world_clone = world.clone();
+                println!("Spawning thread...");
+                thread::spawn(move || {
+                    handle_client(stream, world_clone);
+                });
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        }
     }
+    drop(tcp_listener);
 }
