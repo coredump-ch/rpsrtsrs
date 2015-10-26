2a8bf66f32e17e1cc523b9ab050da30520273eda
Raphael Nestler <raphael.nestler@gmail.com>
2015-10-26 23:22:03 +0100

    state: Add constructor and update method to Unit

--- src/state.rs
+++ src/state.rs
@@ -2,10 +2,25 @@
 #[derive(RustcEncodable, RustcDecodable, PartialEq, Debug, Clone)]
 pub struct Unit {
     pub position: [u64; 2],
-    pub angle: u64,
     pub speed_vector: [u64; 2],
     pub health: u64,
 }
+
+impl Unit {
+    pub fn new(position: [u64;2]) -> Unit {
+        println!("Create unit at {:?}", position);
+        Unit {
+            position: position,
+            speed_vector: [0,0],
+            health: 100_0000,
+        }
+    }
+    pub fn update(&mut self, dt_ms: u64) {
+        self.position[0] += self.speed_vector[0]*dt_ms;
+        self.position[1] += self.speed_vector[1]*dt_ms;
+    }
+}
+
 
 /// A player has an ID and consists of 0..N `Unit`s
 #[derive(RustcEncodable, RustcDecodable, PartialEq, Debug, Clone)]
