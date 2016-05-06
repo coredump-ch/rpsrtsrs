a051fb2d45f0c50a839e8319139cc9cc2045ae56
Danilo Bargen <mail@dbrgn.ch>
2016-05-06 23:07:53 +0200

    Add module documentation

--- src/colors.rs
+++ src/colors.rs
@@ -1,5 +1,12 @@
+//! Color definitions.
+//!
+//! This module should contain all colors used in the game. Later on, we could
+//! replace it with a customizeable version, where people could write themes
+//! (e.g. in TOML or YAML format).
+//!
+//! Make sure to contain only semantic names (e.g. "primary" or "background",
+//! not "red" or "green").
 
 pub const BLACK: [f32; 4] = [0.0, 0.0,  0.0, 1.0];
 pub const YELLOW:[f32; 4] = [1.0, 1.0,  0.22, 1.0];
 pub const ORANGE:[f32; 4] = [1.0, 0.61, 0.22, 1.0];
-
